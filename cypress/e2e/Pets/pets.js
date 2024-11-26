b41688f46f2d08acb5cb098107db15fada767ae4
Lakshmi Jyothi <lakshmi.jyothi@buildium.com>
2024-11-26 19:15:19 +0530

    cypress test

--- cypress/e2e/Pets/pets.js
+++ cypress/e2e/Pets/pets.js
@@ -47,17 +47,17 @@ Given("I have update pet api", () => {
 
 When("I add a new pet in the store", () => {
   const newPetDetails = {
-    id: 0,
+    id: Math.floor(Math.random() * 100000),
     category: {
-      id: 0,
-      name: "pets",
+      id: 1,
+      name: "dog",
     },
     name: "doggie",
     photoUrls: ["/dog/img"],
     tags: [
       {
-        id: 0,
-        name: "dog",
+        id: 1,
+        name: "friendly",
       },
     ],
     status: "available",
@@ -76,17 +76,17 @@ When("I add a new pet in the store", () => {
 
 When("I update the pet name and status", () => {
   cy.get("@newPet").then((newPet) => {
-    const updatedPetDetails = {
-      ...newPet,
-      name: "updatedDoggie",
-      status: "sold",
-    };
+    newPet.name = "updatedDoggie";
+    newPet.status = "sold";
 
     cy.request({
       method: "PUT",
       url: petApiUrl,
-      body: updatedPetDetails,
-    }).as("updateResponse");
+      body: newPet,
+    }).then((response) => {
+      console.log("Update Response:", response);
+      cy.wrap(response).as("updateResponse");
+    });
   });
 });
 
@@ -98,14 +98,106 @@ Then("pet details should be updated successfully", () => {
   });
 });
 
-Then("I should be able to get the pet details", () => {
+Then("I should be able to get all pet details by available", () => {
   cy.request({
     method: "GET",
-    url: "https://petstore.swagger.io/v2/pet",
+    url: "https://petstore.swagger.io/v2/pet/findByStatus?status=available",
   }).then((response) => {
     expect(response.status).to.eq(200);
     expect(response.body.id).to.eq(petId);
     expect(response.body.name).to.eq("doggie");
     expect(response.body.status).to.eq("available");
+  });
+});
+
+When("I added new pet in the store", () => {
+  cy.get("@petApiUrl").then((url) => {
+    const petId = Math.floor(Math.random() * 100000);
+    cy.request({
+      method: "POST",
+      url: url,
+      body: {
+        id: petId,
+        category: {
+          id: 0,
+          name: "pet",
+        },
+        name: "doggie",
+        photoUrls: ["/dog/img"],
+        tags: [
+          {
+            id: 0,
+            name: "dog",
+          },
+        ],
+        status: "available",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      cy.wrap(response.body.id).as("petId");
+    });
+  });
+});
+
+Then("I should be able to get the pet details", () => {
+  cy.get("@petId").then((petId) => {
+    const getPetUrl = `https://petstore.swagger.io/v2/pet/${petId}`;
+    cy.request({
+      method: "GET",
+      url: getPetUrl,
+    }).then((getResponse) => {
+      expect(getResponse.status).to.eq(200);
+      expect(getResponse.body.id).to.eq(petId);
+      expect(getResponse.body.name).to.eq("doggie");
+      expect(getResponse.body.status).to.eq("available");
+    });
+  });
+});
+
+When("I adding new pet in the store", () => {
+  cy.get("@petApiUrl").then((url) => {
+    const petId = Math.floor(Math.random() * 100000);
+    cy.request({
+      method: "POST",
+      url: url,
+      body: {
+        id: petId,
+        category: {
+          id: 0,
+          name: "pet",
+        },
+        name: "doggie",
+        photoUrls: ["/dog/img"],
+        tags: [
+          {
+            id: 0,
+            name: "dog",
+          },
+        ],
+        status: "available",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      cy.wrap(response.body.id).as("pet");
+    });
+  });
+});
+
+Then("I delete the pet", () => {
+  cy.get("@pet").then((petId) => {
+    const deletePetUrl = `https://petstore.swagger.io/v2/pet/${petId}`;
+    cy.request({
+      method: "DELETE",
+      url: deletePetUrl,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      cy.request({
+        method: "GET",
+        url: deletePetUrl,
+        failOnStatusCode: false,
+      }).then((getResponse) => {
+        expect(getResponse.status).to.eq(404);
+      });
+    });
   });
 });
--- cypress/e2e/pets.feature
+++ cypress/e2e/pets.feature
@@ -5,17 +5,20 @@ Feature: Update Pet Details
     When  I add new pet in the store
     Then  pet details should be added successfully
 
- 
  Scenario: Update the name and status of a newly added pet
     Given I have update pet api
     When  I add a new pet in the store
     And   I update the pet name and status 
     Then  pet details should be updated successfully
 
- Scenario: Add a new pet to the store
+ Scenario: Get a new pet to the store
     Given I have add pet api
-    When  I add a new pet in the store
-    Then  pet details should be added successfully
-    And   I should be able to get the pet details
+    When  I added new pet in the store
+    Then  I should be able to get the pet details
+
+ Scenario: Delete the pet from store
+    Given I have add pet api
+    When  I adding new pet in the store
+    Then  I delete the pet
 
   
